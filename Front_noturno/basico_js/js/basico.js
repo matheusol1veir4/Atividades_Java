f7932af3745a5e86911626791c7e6337227bbf0e
Matheus Oliveira <matheus.colvr@gmail.com>
2023-07-13 21:54:50 -0300

    JS

---
+++ Front_noturno/basico_js/js/basico.js
@@ -0,0 +1,21 @@
+console.log("hello world, no arquivo JS!");
+
+console.log( 1+ "1")
+
+console.log( 1+ "1")
+
+var x = 10
+let y = 11
+
+var x = 11
+//let y = 11
+
+console.log(typeof(x))
+
+console.log("aula" * 2)
+
+let numero = "50"
+
+console.log(typeof(numero))
+console.log(typeof(numeroString))
+console.log(typeof(casasDecimais))
