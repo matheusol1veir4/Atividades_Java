7aba979e9ab0a00257d11f1844a4439cf6498971
Matheus Oliveira <matheus.colvr@gmail.com>
2023-08-16 21:50:26 -0300

    meu projeto

--- crudComSpring/src/main/java/br/com/matheus/controller/ProdutoController.java
+++ crudComSpring/src/main/java/br/com/matheus/controller/ProdutoController.java
@@ -1,14 +1,50 @@
 package br.com.matheus.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.matheus.model.Produto;
+import br.com.matheus.repository.ProdutoRepository;
 
 @Controller
 public class ProdutoController {
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@GetMapping("/produto")
-	public String produto () {
+	public String produto (Model requisicao) {
+		requisicao.addAttribute("valor",33);
 		return "produtos";
 	}
+	
+	@GetMapping("/")
+	public String paginaPrincipal () {
+		return "index";
+	}
+	
+	@GetMapping("/cadastrarProduto")
+	public String paginaAdicionarProduto (Produto produto) {
+		return "adicionar_produto";
+	}
+	
+	@PostMapping
+	public String adicionaProduto(@Valid Produto produto,
+			Errors erros, BindingResult result, Model model) {
+		if(result.hasErrors()|| (null != erros && erros.getErrorCount() > 0)) {
+			return "adicionar_produto";
+		}
+		
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
 	
 }
